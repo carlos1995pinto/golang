43efb326a6ee37df939abd03da05db8763c449e0
carlos1995pinto <59972350+carlos1995pinto@users.noreply.github.com>
2020-01-16 22:24:51 +0000

    Add files via upload

--- csv.go
+++ csv.go
@@ -7,60 +7,110 @@ import (
 	//"fmt"
 )
 
-var fid *os.File
-var size1read int = 10
-var fimRead uint64 = 0xFFFFFFFF00000000
-func Initialize(fidIN *os.File) {
-	fid = fidIN
+var fid *os.File //it will save the file descriptor of the csv file
+var size1read int = 10 //it will read 10 bytes of the csv file each time
+var fimRead uint64 = 0xFFFFFFFF00000000 //decode of the begin and end of each field in the all line
+var outString []string //save the fields
+var	currentIDX int = -1 //know which field has been return to the user
+
+//Func :Initialize()
+//Input: file descriptor from the csv file and number of bytes that will be read from the csv file each readI/O call
+//Output: none
+//Objective: save the file descriptor from the file where the function will read and 
+//reset the buffer where the fields will be saved
+func Initialize(fidIN *os.File,readPerCall int) {
+
+	if(readPerCall != 0){
+		size1read = readPerCall
+	}
+	fid = fidIN //save file descriptor
+	outString = nil //reset the buffer where the fields value will be saved
 }
 
+//Func Read1Field()
+//Input: none
+//Output: the field value and a bool
+//Objective: It will return to the user the next  field value. If there is no more field to read from the current 
+//line or a line was never read, then this function will call Read1Line() in order to fill the buffer with new values.
+//If the call to Read1Line() fails (returns a false bool value), than this function returns a empty string and a false bool value.
+//Otherwise, it returns a string with the field value and a true bool value
+func Read1Field() (string,bool){
+
+	currentIDX++ //increase index to get next field value
+	if(outString == nil || currentIDX==len(outString)){ //already output all field values from the current line or i have never read a line from the csv file
+		_,ind := Read1Line() //read a new line
+		currentIDX = 0 //reset the index 
+		if(ind==false){ //something went wrong in the Read1Line() func
+			return "",false
+		}
+		
+	}
+
+	return outString[currentIDX],true //return the field value
+}
+
+//Func: Read1Line()
+//Input: none
+//Output: pointer to a slice of string and a bool
+//Objective: This function will read from the csv file  1 line and will save each field in a slice string. 
+//Each call of this function will read a new line from the csv file. If the progammer calls the Read1Field function and 
+//then calls the Read1Line, the function will return a new line if she exist and will not return the rest of the line that corresponds
+//to the output given by Read1Field.
+//	In case of success this function returns a pointer slice of string that contains each field and a true bool value. 
+//In case of failure, this function returns nil slice of string and a false bool value
 func Read1Line() ([]string,bool) {
-	out,err := read1Line()
-	if(err == false){
+	out,err := read1Line() //read a new line from the csv file
+	if(err == false){ //fail to read a new line. Could be end of file reach or an error read
 		return nil,false
 	}
-	mapCommaQuote := countSpecialChars(out)
-	outString := make([]string,len(mapCommaQuote))
-	var inicioS,fimS uint32
-	var lenV int
-	var lenS uint32 
-	var compensation uint32
+	mapCommaQuote := countSpecialChars(out) //get a map where each key indicates the begin and end of an field value and the values indicate where the char " is present in that field value
+	if(outString == nil){ //th space to save the filed values was never alocate or is a new file that we are dealing
+		outString = make([]string,len(mapCommaQuote)) //allocates the space need to save all the values in the line. This only needs to be done each new file. 
+	}
+	var inicioS,fimS uint32 //begin and end of field value  in the line
+	var lenV int //len of the vetor with the indexs for the " char in the field value
+	var lenS uint32 //len of the field value
+	var compensation uint32 //compensation factor
 	//var lenV int
-	for k,v := range mapCommaQuote{
+	for k,v := range mapCommaQuote{ 
 		lenV = len(v)
-		inicioS = uint32((fimRead >>32) & k)
-		fimS = uint32((fimRead &k)>>32)
-		s:= out[inicioS:fimS]
-		lenS =fimS-inicioS
-		if(lenV!=1){
-			s=s[v[0]+1:v[lenV-2]]
-			compensation = 1
+		inicioS = uint32((fimRead >>32) & k) //decode the begin of the field value in the line
+		fimS = uint32((fimRead &k)>>32) // decode the end of the field value in the line
+		s:= out[inicioS:fimS] //get the field value
+		lenS =fimS-inicioS //get the len of the field value
+		if(lenV!=1){ //the last value in the vetor V is the order of the field value in the line so all vector values from the map have at least one value
+			s=s[v[0]+1:v[lenV-2]] //if it has more, than it means the field value has the " character. So we remove the first and the last which happens everytime that a field value has " and ; characters			compensation = 1
 		}
 
-		for i:=1;i<lenV-2;i+=2{ //agora só existem pares 
-			copy(s[v[i]-compensation: lenS-compensation-2],s[v[i+1]-compensation:])
-			s = s[:lenS-compensation-2]
-			compensation++
+		for i:=1;i<lenV-2;i+=2{ //eliminate the others " characters. Remind that each " insert by the user in the csv file, it will be saved in the csv file with the "" characters
+			copy(s[v[i]-compensation: lenS-compensation-2],s[v[i+1]-compensation:]) //remove one of the " character from the pair
+			s = s[:lenS-compensation-2] //erases the extra " character
+			compensation++ //increase the compensation factor because the len of the field value has changed
 		}
-		outString[v[lenV-1]]=string(s)
-		//fmt.Printf("%s\n",outString[v[lenV-1]])
+		outString[v[lenV-1]]=string(s) //convert the field value that is in bytes to a string and save him in the right order
+		
 	}
-	//var StringCount,inicio,fim int
+	currentIDX = len(outString)
 	return outString,true
 }
 
+//Func: read1Line()
+//Input: none
+//Output: byte slice and a bool value
+//Objective: read a new line from the csv file. In sucess the function returns the slice of bytes read that
+//corresponds to a new line and a true bool value. In case of failure, the function returns a nil slice and a false bool value
 func read1Line() ([]byte, bool) {
 	if fid == nil {
 		return nil, false
 	}
-	origPos, _ := fid.Seek(0, 1)
-	conteudo := make([]byte, size1read, 2*size1read)
-	n, err := fid.Read(conteudo)
-	if err != nil && n == 0{        
+	origPos, _ := fid.Seek(0, 1) //gets the current position of the csv read pointer
+	conteudo := make([]byte, size1read, 2*size1read) //allocates size1read bytes but with twice the capacity. 
+	n, err := fid.Read(conteudo) //read size1read bytes from the file
+	if err != nil && n == 0{         //didn't have nothing to read or a read error has occur
 		return nil, false
 	}
 	var idx, nPrev, contagem int = 0, n, 0
-	aux := make([]byte, size1read)
+	aux := make([]byte, size1read) //auxiliar buffer
 	for idx = bytes.IndexAny(conteudo[contagem:n+contagem], "\r\n"); idx < 0; idx = bytes.IndexAny(conteudo[contagem:n+contagem], "\r\n") {
 		if err == io.EOF {
 			break
